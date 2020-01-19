b00993b1214566c71263def5cf70a60dc4fce434
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-19 20:32:26 +0000

    Infix to postfix convertion

--- src/index.php
+++ src/index.php
@@ -1,9 +1,56 @@
 <?php
+require_once('models/Stack.php');
+
 $view = new stdClass();
 $view->pageTitle = 'Calculator';
 
 if (isset($_GET['submit'])) {
+    $stack = new Stack();
+    $infix = $_GET['expression'];
+    $postfix = "";
+    $array = str_split($infix);
 
+    for ($i = 0; $i < count($array); $i++) {
+        $char = $array[$i];
+
+        if($stack->isOperand($char)) {
+            $postfix .= $char;
+        }
+        elseif ($char == "(") {
+            $stack->push($char);
+        }
+        elseif ($char == ")") {
+            while (!$stack->isEmpty() && $stack->top() != "(") {
+                $postfix .= $stack->pop();
+            }
+
+            if(!$stack->isEmpty() && $stack->top() != "(") {
+                $postfix = "Invalid infix expression";
+            }
+            else {
+                $stack->pop();
+            }
+        }
+        elseif ($stack->isOperator($char)){
+            if(!$stack->isEmpty() && $stack->findPrecedence($char) <= $stack->findPrecedence($stack->top())) {
+                $postfix .= $stack->pop();
+            }
+            $stack->push($char);
+        }
+        else {
+            $postfix = "Invalid infix expression";
+        }
+    }
+    while(!$stack->isEmpty()) {
+        if($stack->top() == "(") {
+            $postfix = "Invalid infix expression";
+        }
+        $postfix .= $stack->pop();
+    }
+
+    $view->postfix = $postfix;
 }
+
+
 
 require_once('views/index.phtml');
--- src/models/Stack.php
+++ src/models/Stack.php
@@ -2,8 +2,8 @@
 
 
 class Stack {
-    protected $stack;
-    protected $limit;
+    private $stack;
+    private $limit;
 
     public function __construct($limit = 20) {
         // initialize the stack
@@ -38,5 +38,36 @@ class Stack {
 
     public function isEmpty() {
         return empty($this->stack);
+    }
+
+    public function isOperator($char) {
+        if($char == "+" || $char == "-" || $char == "*" || $char == "/" || $char == "^") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function isOperand($char) {
+        return is_numeric($char);
+    }
+
+    public function findPrecedence($char) {
+        switch ($char){
+            case "+":
+            case "-":
+                return 1;
+                break;
+            case '*':
+            case '/':
+                return 2;
+                break;
+            case '^':
+                return 3;
+                break;
+            default:
+                return -1;
+        }
     }
 }
