da998a19e72087402aa199be0696dd72ae51d0a6
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-19 14:52:10 +0000

    Basic calculator

--- src/index.php
+++ src/index.php
@@ -2,4 +2,31 @@
 $view = new stdClass();
 $view->pageTitle = 'Calculator';
 
+if (isset($_GET['submit'])) {
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+    $operator = $_GET['operator'];
+    $result = 0;
+
+    switch ($operator) {
+        case "None":
+            echo "Chose an operator.";
+        break;
+        case "add":
+            $result = $num1 + $num2;
+            break;
+        case "subtract":
+            $result = $num1 - $num2;
+            break;
+        case "multiply":
+            $result = $num1 * $num2;
+            break;
+        case "divide":
+            $result = $num1 / $num2;
+            break;
+    }
+
+    $view->result = $result;
+}
+
 require_once('views/index.phtml');
