48c4fa51e534d45938ce81fc75053b4c3ad8cb04
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-19 23:24:56 +0000

    Exponentiation added

--- src/index.php
+++ src/index.php
@@ -66,9 +66,11 @@ if (isset($_GET['submit'])) {
     for ($i = 0; $i < count($expression); $i++) {
         $s = $expression[$i];
 
+        // If the scanned character is a number push it to the stack
         if(is_numeric($s)) {
             $evaluation->push($s);
         }
+        // If the scanned character is an operator, pop two elements from stack apply the operator
         else {
             $num1 = $evaluation->pop();
             $num2 = $evaluation->pop();
@@ -86,17 +88,16 @@ if (isset($_GET['submit'])) {
                 case '/':
                     $evaluation->push($num2 / $num1);
                     break;
-//                case '^':
-//                    $evaluation->push($num1 $num2);
-//                    break;
+                case '^':
+                    $evaluation->push($num2 ** $num1);
+                    break;
             }
         }
     }
 
     $result = $evaluation->pop();
+    //display the result
     $view->result = $result;
-
-    //$view->postfix = $postfix;
 }
 
 
