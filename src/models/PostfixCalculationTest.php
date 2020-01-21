69d92e9959b5479058d08e33ac3b78f5b3ee758a
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-21 20:49:36 +0000

    One more test for postfixCalculation

--- src/models/PostfixCalculationTest.php
+++ src/models/PostfixCalculationTest.php
@@ -22,4 +22,25 @@ class PostfixCalculationTest extends TestCase {
             ["79*2326^*+*"]
         ];
     }
+
+    /**
+     * @dataProvider dataProvider
+     * @param $postfix
+     * @param $expected
+     */
+    public function testPostfixCalculationForResult($postfix, $expected) {
+        $postfixCalculation = new PostfixCalculation();
+        $result = $postfixCalculation->postfixCalculation($postfix);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function dataProvider()
+    {
+        return [
+            ["1591-*+", "41"],
+            ["13+64-*", "8"],
+            ["25*84/+", "12"],
+            ["79*2326^*+*", "12222"]
+        ];
+    }
 }
