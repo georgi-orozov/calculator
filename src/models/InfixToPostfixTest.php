8d96e2e0c9cbaa13d81cb7fb2f4c707df3d246f8
Nikolay Orozov <dev@niko-consulting.de>
2020-01-21 21:48:07 +0100

    couple of simple unit tests

--- src/models/InfixToPostfixTest.php
+++ src/models/InfixToPostfixTest.php
@@ -5,9 +5,9 @@ use PHPUnit\Framework\TestCase;
 
 class InfixToPostfixTest extends TestCase {
     /**
-     * @dataProvider additionProvider
+     * dataProvider additionProvider
      * @param $example
-     */
+     *
     public function testInfixToPostfixForReturnType($example) {
         $infixToPostfix = new InfixToPostfix();
         $result = $infixToPostfix->infixToPostfix($example);
@@ -23,4 +23,30 @@ class InfixToPostfixTest extends TestCase {
             ["(7*9)*(2+3*(2^6))"]
         ];
     }
+    */
+
+    public function testInfixToPostfixSimple() {
+        $infixToPostfix = new InfixToPostfix();
+        $infix = "1+0";
+        $postfix = $infixToPostfix->infixToPostfix($infix);
+        $this->assertIsString($postfix);
+        $this->assertEquals("10+", $postfix);
+    }
+
+    public function testInfixToPostfix() {
+        $infixToPostfix = new InfixToPostfix();
+        $infix = "1+2*3";
+        $postfix = $infixToPostfix->infixToPostfix($infix);
+        $this->assertIsString($postfix);
+        $this->assertEquals("123*+", $postfix);
+    }
+
+    public function testInfixToPostfixAdvanced() {
+        $infixToPostfix = new InfixToPostfix();
+        $infix = "(7*9)*(2+3*(2^6))";
+        $postfix = $infixToPostfix->infixToPostfix($infix);
+        $this->assertIsString($postfix);
+        $this->assertEquals("79*2326^*+*", $postfix);
+    }
+
 }
